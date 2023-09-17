3f649b8461c92365ec92ca763cb466020cecbe87
Jihee <42454759+sjh709@users.noreply.github.com>
2023-09-17 16:59:56 +0900

    JS-Home

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector(".home__container");
+const homeHeight = home.offsetHeight;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
